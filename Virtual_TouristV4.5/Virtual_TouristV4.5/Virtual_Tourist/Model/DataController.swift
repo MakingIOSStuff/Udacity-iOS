43fff6f7327dfe58d24dda5620e1e05c437dc74b
MakingIOSStuff <111947266+MakingIOSStuff@users.noreply.github.com>
2022-09-11 15:01:14 -0700

    Add files via upload

---
+++ Virtual_TouristV4.5/Virtual_TouristV4.5/Virtual_Tourist/Model/DataController.swift
@@ -0,0 +1,65 @@
+//
+//  DataController.swift
+//  Virtual_Tourist
+//
+//  Created by Joel Gans on 8/28/22.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    
+    let persistentContainer: NSPersistentContainer
+    var viewContext: NSManagedObjectContext {
+            return persistentContainer.viewContext
+        }
+    let backgroundContext: NSManagedObjectContext!
+    
+    init(modelName: String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+        backgroundContext = persistentContainer.newBackgroundContext()
+    }
+    
+    func configureContexts() {
+        viewContext.automaticallyMergesChangesFromParent = true
+        backgroundContext.automaticallyMergesChangesFromParent = true
+        
+        viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
+        backgroundContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+    }
+    
+    func load(completion: (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { storeDescription, error in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+//                return
+            }
+            self.autoSaveViewContext()
+            self.configureContexts()
+            completion?()
+        }
+    }
+}
+
+//MARK: Auto Save Functionality
+
+extension DataController {
+    func autoSaveViewContext(interval: TimeInterval = 30) {
+        print("Auto Saving")
+        
+        guard interval > 0 else {
+            print("Cannot save at negative time intervals")
+            return
+        }
+        //Check for changes before saving
+        if viewContext.hasChanges {
+            try? viewContext.save()
+        }
+        //Begin auto saving
+        DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+            self.autoSaveViewContext(interval: interval)
+        }
+    }
+}
+
