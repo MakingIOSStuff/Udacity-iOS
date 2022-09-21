ded88a96630fcfec30176bee7d7e423a48f6296a
MakingIOSStuff <111947266+MakingIOSStuff@users.noreply.github.com>
2022-09-21 13:55:39 -0700

    Add files via upload

---
+++ Inspirational Bytes/Inspirational Bytes/Controllers/QuoteViewController.swift
@@ -0,0 +1,98 @@
+//
+//  QuoteViewController.swift
+//  Inspirational Bytes
+//
+//  Created by Joel Gans on 9/11/22.
+//
+
+import UIKit
+import CoreData
+
+class QuoteViewController: UIViewController, NSFetchedResultsControllerDelegate {
+
+    @IBOutlet weak var QuoteLabel: UILabel!
+    @IBOutlet weak var AuthorLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    let backgroundImageDownloadQueue = DispatchQueue(label: "com.vt.quote_download_queue")
+    var favQuoteText: String = ""
+    var author: String = ""
+    var fromFavorites: Bool = false
+    var dataController: DataController = (UIApplication.shared.delegate as! AppDelegate).dataController
+    var quote: Quote!
+    var fetchedResultsController: NSFetchedResultsController<Quote>!
+    
+    func setupFetchedResultsController() {
+        
+        let fetchRequest:NSFetchRequest<Quote> = Quote.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "authorName", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController.delegate = self
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("The fetch could not be performed: \(error.localizedDescription)")
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        setupFetchedResultsController()
+        super.viewDidLoad()
+        activityIndicator.startAnimating()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        if fromFavorites {
+            QuoteLabel.text = favQuoteText
+            AuthorLabel.text = author
+            activityIndicator.stopAnimating()
+        } else {
+            setSavedQuote()
+        }
+    }
+    
+    func setSavedQuote() {
+        if fetchedResultsController.fetchedObjects?.count ?? 0 < 5 {
+            NetworkManager.getQuotes { quoteResponse, error in
+                if error != nil {
+                    var currentQuote = quoteResponse[0]
+                    self.QuoteLabel.text = currentQuote.text
+                    self.AuthorLabel.text = currentQuote.author
+                    try? self.dataController.viewContext.save()
+                    self.setupFetchedResultsController()
+                }
+            }
+        } else {
+            var currentQuote = fetchedResultsController.fetchedObjects?[0]
+            QuoteLabel.text = currentQuote?.quoteText
+            AuthorLabel.text = currentQuote?.authorName
+        }
+        activityIndicator.stopAnimating()
+    }
+    
+    @IBAction func NewQuoteButton(_ sender: Any) {
+        guard let item = fetchedResultsController.fetchedObjects?[0] else { return }
+        dataController.viewContext.delete(item)
+        try? dataController.viewContext.save()
+        reloadInputViews()
+        activityIndicator.startAnimating()
+        setSavedQuote()
+    }
+
+    @IBAction func backButtonPressed(_ sender: Any) {
+        self.navigationController?.popToRootViewController(animated: true)
+        self.dismiss(animated: true)
+    }
+    
+    //MARK: Display error
+    func showError(message: String) {
+        let alertVC = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        show(alertVC, sender: nil)
+    }
+}
+
---
+++ Inspirational Bytes/Inspirational Bytes/Managers/NetworkManager.swift
@@ -0,0 +1,103 @@
+//
+//  NetworkManager.swift
+//  Inspirational Bytes
+//
+//  Created by Joel Gans on 9/16/22.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    enum Endpoints {
+        static let base = "https://zenquotes.io/api/"
+
+        case randomQuotesRequest
+        case QOTD
+        
+        var stringValue: String {
+            switch self {
+            case .randomQuotesRequest:
+                return Endpoints.base + "quotes"
+            case .QOTD:
+                return Endpoints.base + "today"
+            }
+        }
+        var url: URL {
+            return URL(string: stringValue)!
+        }
+    }
+    
+    @discardableResult class func taskForGETRequest<ResponseType: Decodable>(url: URL, responseType: ResponseType.Type, completion: @escaping (ResponseType?, Error?) -> Void) -> URLSessionDataTask {
+          let task = URLSession.shared.dataTask(with: url) { data, response, error in
+              debugPrint("Tried the get and was got: \(String(describing: response))")
+              guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                  print("Error with the response, unexpected status code: \(String(describing: response))")
+                  return
+              }
+              guard let data = data else {
+                  debugPrint("The guard statement prevented DECODE")
+                  DispatchQueue.main.async {
+                      completion(nil, error)
+                  }
+                  return
+              }
+              let decoder = JSONDecoder()
+              do {
+                  debugPrint("We are going to decode now with: \(data) that is \(String(describing: data))")
+                  debugPrint(String(data: data, encoding: .utf8)!)
+                  let responseObject = try decoder.decode(responseType, from: data)
+                  print("responseObject: \(responseObject)")
+                  DispatchQueue.main.async {
+                      completion(responseObject, nil)
+                  }
+              } catch {
+                  do {
+                      debugPrint("Failed to decode")
+                      let errorResponse = try decoder.decode([QuoteResponse].self, from: data) as! Error
+                      DispatchQueue.main.async {
+                          completion(nil, errorResponse)
+                      }
+                  } catch {
+                      debugPrint("Failed to decode")
+                      DispatchQueue.main.async {
+                          completion(nil, error)
+                      }
+                  }
+              }
+          }
+          task.resume()
+          
+          return task
+      }
+    
+   class func getQuotes(completion: @escaping ([QuoteResponse], Error?) -> Void) {
+        NetworkManager.taskForGETRequest(url: Endpoints.randomQuotesRequest.url, responseType: [QuoteResponse].self) { response, error in
+            print("requested URL: \(Endpoints.randomQuotesRequest.url)")
+            if let response = response {
+                DispatchQueue.main.async {
+                    completion(response, nil)
+                }
+            } else {
+                completion([], error)
+            }
+        }
+    }
+    
+    class func getQOTD(completion: @escaping ([QuoteResponse]?, Error?) -> Void) {
+        NetworkManager.taskForGETRequest(url: Endpoints.QOTD.url, responseType: [QuoteResponse].self) { response, error in
+            print("requested URL: \(Endpoints.QOTD.url)")
+            if let response = response {
+                DispatchQueue.main.async {
+                    completion(response, nil)
+                }
+            } else {
+                completion(nil ,error)
+            }
+        }
+    }
+        
+        
+}
